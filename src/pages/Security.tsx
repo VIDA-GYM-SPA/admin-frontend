1814d0211e9eda89a2d4c041c61f93143f062e0f
EvananSemprun <evanansemprun@gmail.com>
2023-09-29 17:43:23 -0400

    Security update

--- src/pages/Security.tsx
+++ src/pages/Security.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Card,
   Table,
@@ -7,7 +8,6 @@ import {
   Breadcrumbs,
   Anchor,
   Input,
-  Button,
   ActionIcon,
   Pagination
 } from "@mantine/core"
@@ -21,34 +21,49 @@ import EditModal from "../components/_security/EditModal";
 import DeleteModal from "../components/_security/DeleteModal";
 import Addclient from "../components/_security/Addclient";
 
-interface IElement {
-  suscripcion: number;
-  rol: string;
-  pulsera: string;
-  Huella: string;
-  nombre: string;
-}
 
 function Security() {
+  const [searchValue, setSearchValue] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const rowsPerPage = 17;
+
   const elements = [
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Diego Urrutia' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Activa', nombre: 'Evanan Semprun' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Maria Rodriguez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Desactivada', nombre: 'Carlos Gomez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Laura Hernandez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Juan Perez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Desactivada', nombre: 'Ana Martinez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Pedro Sanchez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Activa', nombre: 'Isabel Torres' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Luis Ramirez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Desactivada', nombre: 'Carlos Gomez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Laura Hernandez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Juan Perez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Desactivada', nombre: 'Ana Martinez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Pedro Sanchez' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Activa', nombre: 'Isabel Torres' },
-    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Luis Ramirez' },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Diego Urrutia' ,cedula:29543321},
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Activa', nombre: 'Evanan Semprun',cedula:29543546 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Maria Rodriguez',cedula:295434534 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Desactivada', nombre: 'Carlos Gomez',cedula:28543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Laura Hernandez',cedula:19543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Juan Perez',cedula:29543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Desactivada', nombre: 'Ana Martinez',cedula:29543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Pedro Sanchez' ,cedula:29543321},
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Activa', nombre: 'Isabel Torres',cedula:29543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Luis Ramirez' ,cedula:29543321},
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Desactivada', nombre: 'Carlos Gomez' ,cedula:29543321},
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Laura Hernandez',cedula:29543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Juan Perez',cedula:29543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Desactivada', nombre: 'Ana Martinez',cedula:29543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Pedro Sanchez',cedula:29543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Activa', nombre: 'Isabel Torres' ,cedula:29543321},
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Luis Ramirez' ,cedula:20543321},
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Desactivada', nombre: 'Ana Martinez',cedula:29543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Pedro Sanchez',cedula:29543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Activa', nombre: 'Isabel Torres' ,cedula:28543321},
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Luis Ramirez' ,cedula:29543321},
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Desactivada', nombre: 'Ana Martinez' ,cedula:29543321},
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Desactivada', nombre: 'Pedro Sanchez',cedula:24543321 },
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Desactivada', Huella: 'Activa', nombre: 'Isabel Torres' ,cedula:29543321},
+    { suscripcion: 35, rol: "Cliente", pulsera: 'Activa', Huella: 'Activa', nombre: 'Luis Ramirez' ,cedula:29543321},
   ];
+  const filteredElements = elements.filter((element) =>
+    element.nombre.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  const totalPages = Math.ceil(filteredElements.length / rowsPerPage);
+  const startIndex = (currentPage - 1) * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+  const currentRows = filteredElements.slice(startIndex, endIndex);
+
+
   const plink = [
     { title: 'Dashboard', href: '/' },
     { title: 'Seguridad', href: '#' },
@@ -58,9 +73,11 @@ function Security() {
     </Anchor>
   ));
 
-  const rows = elements.map((element: IElement) => (
-    <tr>
+
+  const rows = currentRows.map((element, index) => (
+    <tr key={index}>
       <td>{element.nombre}</td>
+      <td>{element.cedula}</td>
       <td>{element.suscripcion}$</td>
       <td>{element.rol}</td>
       <td style={{ textAlign: "center" }}>
@@ -95,6 +112,9 @@ function Security() {
     </tr>
   ));
 
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
   return (
     <>
       <Card
@@ -118,33 +138,43 @@ function Security() {
         </Title>
         <Group position="apart">
 
-        <Group w={955}>
-          <Input
-            maw={500}
-            mt={10}
-            size="md"
-            icon={<IconSearch />}
-            placeholder="Nombre o Cedula"
-          />
+          <Group w={955} spacing={0} >
+            <Input
+              maw={500}
+              mt={10}
+              size="md"
+              icon={<IconSearch />}
+              placeholder="Nombre"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
 
-          <Group spacing={0} mt={10}>
-            <Button color="blue" variant="filled" size="md" style={{ borderRadius: '5px 0 0 5px'}}>Filtrar</Button>
-            <ActionIcon px={3} h={42} color="orange" variant="filled" style={{ borderRadius: '0 5px 5px 0'}}>
-              <IconRepeat />
-            </ActionIcon>
+
+            <Group spacing={0} mt={10}>
+              {/* <Button color="blue" variant="filled" size="md" style={{ borderRadius: '5px 0 0 5px' }}>Filtrar</Button> */}
+              <ActionIcon px={3} h={42} color="orange" variant="filled" style={{ borderRadius: '0 5px 5px 0' }}>
+                <IconRepeat />
+              </ActionIcon>
+
+            </Group>
 
           </Group>
+          <Addclient />
 
         </Group>
-<Addclient/>
 
-</Group>
-
-        <Pagination mt={15} total={10} size="md"/>
+        <Pagination
+          mt={15}
+          total={totalPages}
+          value={currentPage}
+          size="md"
+          onChange={handlePageChange}
+        />
         <Table mt={15} highlightOnHover withBorder withColumnBorders ta="center">
           <thead>
             <tr>
               <th style={{ textAlign: 'center' }}>Nombre</th>
+              <th style={{ textAlign: 'center' }}>Cedula</th>
               <th style={{ textAlign: 'center' }}>Suscripcion</th>
               <th style={{ textAlign: 'center' }}>Rol</th>
               <th style={{ textAlign: 'center' }}>Pulsera</th>
@@ -155,7 +185,13 @@ function Security() {
           <tbody>{rows}</tbody>
         </Table>
 
-        <Pagination mt={15} total={10} size="md"/>
+        <Pagination
+          mt={15}
+          total={totalPages}
+          value={currentPage}
+          size="md"
+          onChange={handlePageChange}
+        />
 
 
       </Card>
